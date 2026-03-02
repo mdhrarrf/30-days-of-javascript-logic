092f1026b515fddaec7252606c0b7e39a6590905
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-02 21:49:44 +0700

    soal 2

---
+++ 01-building-blocks/day-01-variables/02-primitive-test.js
@@ -0,0 +1,10 @@
+let nama1 = "Budi";
+let nama2 = nama1;
+nama2 = "Andi";
+
+console.log("Nama 1:", nama1);
+console.log("Nama 2:", nama2);
+
+// Jawab:
+// Menurut saya Nama 1 isinya adalah: Budi
+// Alasannya: Karena mengambil variable yang paling atas, kecuali klo yg nama 2
