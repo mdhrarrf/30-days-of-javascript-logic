a17472f58932f866373ef7e0a972644cc181397d
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-03 09:36:06 +0700

    day2

---
+++ 01-building-blocks/day-02-conditionals/01-strict-equality.js
@@ -0,0 +1,6 @@
+const angka = 10;
+const teks = "10";
+
+console.log(angka == teks);  // Output? true
+console.log(angka === teks); // Output? false
+// Alasan: karena yang pertama itu hanya ngecek value nya saja, tapi kalau yg kedua ngecek tipe datanya juga
---
+++ 01-building-blocks/day-02-conditionals/02-cek-kelulusan.js
@@ -0,0 +1,12 @@
+const nilaiUjian = 80;
+const nilaiKehadiran = 70;
+
+// TULIS LOGIKA IF ELSE DI SINI
+// Jika lulus cetak: "Selamat anda lulus!"
+// Jika tidak lulus cetak: "Maaf, anda harus mengulang."
+
+if (nilaiUjian >= 75 && nilaiKehadiran >= 80) {
+    console.log("Selamat anda lulus!");
+} else {
+    console.log("Maaf, anda harus mengulang.");
+}
