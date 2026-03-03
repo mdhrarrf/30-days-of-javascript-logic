2b9afff97d5b8e2aa5a2adf59999d32a54e07c04
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-04 05:58:54 +0700

    selesai day 3

---
+++ 01-building-blocks/day-03-loops/03-total-penjumlahan.js
@@ -0,0 +1,13 @@
+let total = 0;
+
+for (let i = 0; i <= 10; i++) {
+    if (i > 0) {
+        console.log("+");
+    }
+    console.log(i);
+
+    total += i;
+}
+
+console.log ("=");
+console.log (total);
