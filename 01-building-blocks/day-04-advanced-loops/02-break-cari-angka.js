f1bb3f5fd8c5209ee60ffd720cbfff97d21fb8a3
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-05 19:47:32 +0700

    for logic

---
+++ 01-building-blocks/day-04-advanced-loops/02-break-cari-angka.js
@@ -0,0 +1,7 @@
+for (let angka = 0; angka <= 100; angka++) {
+    if (angka === 33) {
+        console.log("Angka 33 ditemukan, loop dihentikan!");
+        break;
+    }
+    console.log("Angka saat ini:", angka);
+}
