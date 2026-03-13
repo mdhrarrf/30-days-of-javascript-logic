e00906e8aeea6fce05d1a3064d904d58f876d619
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-13 14:42:51 +0700

    function salam

---
+++ 02-data-structures/day-08-functions/01-function-salam.js
@@ -0,0 +1,7 @@
+function sapaUser(nama) {
+    let sapa = `Halo ${nama}, selamat belajar JavaScript!`;
+    return sapa;
+}
+
+let sapa1 = sapaUser("Muhammad Haidar Almer Rafif");
+console.log(sapa1);
---
+++ 02-data-structures/day-08-functions/02-luas-persegi.js
@@ -0,0 +1,7 @@
+// function hitungPersegi(sisi) {
+//     let hitung = sisi * sisi;
+//     return hitung;
+// }
+
+// let hitung1 = hitungPersegi(10);
+// console.log(hitung1);
