a0ba160e88d11d842db3118b8c15270a2e2b1032
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-19 17:33:43 +0700

    array methods

---
+++ 02-data-structures/day-10-array-methods/01-cetak-hobi.js
@@ -0,0 +1,7 @@
+const hobi = ["Coding", "Reading", "Gaming", "Hiking"];
+
+hobi.forEach(function(namaHobi) {
+    console.log(`Hobi saya adalah ${namaHobi}`);
+});
+
+hobi.forEach(namaHobi => console.log(`Hobi saya adalah ${namaHobi}`));
