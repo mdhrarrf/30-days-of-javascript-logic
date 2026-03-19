9bc128204a2dbcdf0a56d90e62ee8123de64ccdd
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-19 17:49:48 +0700

    buku fac

---
+++ 02-data-structures/day-11-objects/01-buku-favorit.js
@@ -0,0 +1,9 @@
+const buku = {
+    judul : "Laskar Pelangi",
+    penulis : "Andrea Hirata",
+    tahunTerbit : 2000,
+    isBestSeller : true
+}
+
+console.log(buku.judul);
+console.log(buku.penulis);
