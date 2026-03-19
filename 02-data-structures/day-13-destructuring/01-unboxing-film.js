9cdd9341b5a2088463d10a26e95525fe748defb6
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-20 00:48:27 +0700

    unboxing film

---
+++ 02-data-structures/day-13-destructuring/01-unboxing-film.js
@@ -0,0 +1,10 @@
+const film = {
+    judul: "Inception",
+    sutradara: "Christopher Nolan",
+    tahun: 2010,
+    genre: "Sci-Fi"
+};
+
+const { judul, sutradara, tahun } = film;
+
+console.log(`Film "${judul}" disutradarai oleh ${sutradara} pada tahun ${tahun}.`);
