e5f81384a05c4056f35337d546a48a886654c0e3
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-19 17:07:40 +0700

    day 9

---
+++ 02-data-structures/day-09-scope/01-global-vs-local.js
@@ -0,0 +1,9 @@
+let nama = "Almer";
+
+function gantiNama() {
+    let nama = "Haidar";
+    console.log("Di dalam fungsi:", nama); // Ini Haidar
+}
+
+gantiNama();
+console.log("Di luar fungsi:", nama); // Ini Almer 
