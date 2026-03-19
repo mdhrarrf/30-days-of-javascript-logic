9aad84228ba00302c308a838659375b5ad45ebe5
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-19 23:40:25 +0700

    ubah data objek

---
+++ 02-data-structures/day-11-objects/02-ubah-data-objek.js
@@ -0,0 +1,10 @@
+const siswa = {
+    nama : "Muhammad Haidar Almer Rafif",
+    kelas : "XI PPLG-RPL 2",
+    skor : 80
+}
+
+siswa.skor = 95;
+siswa.sekolah = "SMKN 1 Cianjur";
+
+console.log(siswa);
