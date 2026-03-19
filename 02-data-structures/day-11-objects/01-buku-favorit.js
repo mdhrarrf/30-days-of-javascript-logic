04cd302967bd93ebb58543317529b3153309f3ec
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-19 23:43:28 +0700

    bersarang objek

--- 02-data-structures/day-11-objects/01-buku-favorit.js
+++ 02-data-structures/day-11-objects/01-buku-favorit.js
@@ -3,7 +3,7 @@ const buku = {
     penulis : "Andrea Hirata",
     tahunTerbit : 2000,
     isBestSeller : true
-}
+};
 
 console.log(buku.judul);
 console.log(buku.penulis);
---
+++ 02-data-structures/day-11-objects/03-objek-bersarang.js
@@ -0,0 +1,9 @@
+const laptop = {
+    merek : "Asus",
+    spesifikasi : {
+        processor : "Core i7",
+        ram : "16GB"
+    }
+};
+
+console.log(laptop.spesifikasi.ram);
