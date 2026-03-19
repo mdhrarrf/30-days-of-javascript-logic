c898feeca6b8da55ed9817f9e2afcbe79571292e
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-20 01:06:59 +0700

    feat: master destructuring and professional templates day 13

---
+++ 02-data-structures/day-13-destructuring/04-html-template.js
@@ -0,0 +1,10 @@
+const [namaProduk, hargaProduk] = ["Laptop Gaming", 15000000];
+
+const template = `
+    <div>
+        <h1>Nama: ${namaProduk}</h1>
+        <p>Harga: Rp${hargaProduk}</p>
+    </div>
+`;
+
+console.log(template);
