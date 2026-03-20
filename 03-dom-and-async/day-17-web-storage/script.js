219b03feb8f82ebf02e69dd2144a273e5326cd3f
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-20 18:42:19 +0700

    event simpan

--- 03-dom-and-async/day-17-web-storage/script.js
+++ 03-dom-and-async/day-17-web-storage/script.js
@@ -1,10 +1,27 @@
-// SOAL 1
-const tombolSimpan = document.getElementById('btn-simpan');
-const inputNama = document.getElementById('input-nama');
+// 1. TANGKAP SEMUA ELEMEN
 const greeting = document.getElementById('greeting');
+const inputNama = document.getElementById('input-nama');
+const tombolSimpan = document.getElementById('btn-simpan');
 
-tombolSimpan.addEventListener('click', function() {
-    localStorage.setItem('namaUser', 'input-nama');
+// 2. CEK INGATAN (SOAL 2)
+const namaTersimpan = localStorage.getItem('namaUser');
+if (namaTersimpan) {
+    greeting.innerText = `Halo lagi, ${namaTersimpan}!`;
+}
 
-    greeting.innerText = `Halo! ${inputNama.value}`;
+// 3. EVENT SIMPAN (SOAL 1)
+tombolSimpan.addEventListener('click', function () {
+    const namaBaru = inputNama.value;
+    
+    localStorage.setItem('namaUser', namaBaru);
+
+    greeting.innerText = `Halo ${namaBaru}`;
+});
+
+const tombolHapus = document.getElementById('btn-hapus');
+
+tombolHapus.addEventListener('click', function() {
+    localStorage.removeItem('namaUser');
+
+    greeting.innerText = 'Halo, orang asing!';
 });
