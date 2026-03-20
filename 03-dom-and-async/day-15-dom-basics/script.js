8ae68df491ef74b3af4d6b13d5d661fd3045f167
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-20 15:15:36 +0700

    ganti style

--- 03-dom-and-async/day-15-dom-basics/script.js
+++ 03-dom-and-async/day-15-dom-basics/script.js
@@ -1,2 +1,11 @@
 // --- SOAL 1: Ganti Teks ---
-document.getElementById('main-title').innerText = "Halo Dunia";
+const judul = document.getElementById('main-title')
+
+judul.innerText = "Halo Dunia";
+
+// --- SOAL 2: Ganti Gaya (Style) ---
+const kotak = document.getElementById('box')
+
+kotak.style.backgroundColor = "royalblue";
+kotak.style.height = "200px";
+kotak.style.width = "200px";
