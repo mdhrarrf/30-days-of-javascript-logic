985003448a62aee95380b84f59734b6eee9ab748
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-20 15:20:17 +0700

    tambahkan html

--- 03-dom-and-async/day-15-dom-basics/script.js
+++ 03-dom-and-async/day-15-dom-basics/script.js
@@ -1,11 +1,16 @@
 // --- SOAL 1: Ganti Teks ---
-const judul = document.getElementById('main-title')
+const judul = document.getElementById('main-title');
 
 judul.innerText = "Halo Dunia";
 
 // --- SOAL 2: Ganti Gaya (Style) ---
-const kotak = document.getElementById('box')
+const kotak = document.getElementById('box');
 
 kotak.style.backgroundColor = "royalblue";
 kotak.style.height = "200px";
 kotak.style.width = "200px";
+
+// --- SOAL 3: Tambah HTML ---
+const deskripsi = document.querySelector('.description');
+
+deskripsi.innerHTML = "Teks ini <u>sudah diganti</u> lewat JavaScript!";
